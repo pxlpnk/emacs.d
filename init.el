3e0374371856fe69130e9b526b81c2a34a768e12
at <at@an-ti.eu>
2012-10-14 23:18:08 +0200

    adding haskell mode and updating org mode

--- init.el
+++ init.el
@@ -77,6 +77,7 @@
 (load-local-file "etc/full-ack.el")
 (load-local-file "etc/ruby-mode.el")
 (load-local-file "etc/fly-make-ruby.el")
+(load-local-file "etc/haskell-mode.el")
 
 (load-local-file "etc/keys.el")
 ;; backups
@@ -90,3 +91,16 @@
 
 
 (server-start)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(send-mail-function (quote mailclient-send-it)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'downcase-region 'disabled nil)
