1a0f0804bd134c4ef4a370e524ee4ee06fecc915
Andreas Tiefenthaler <andreas.tiefenthaler@radarservices.eu>
2012-10-15 15:02:49 +0200

    orgmode foo

--- etc/org-mode.el
+++ etc/org-mode.el
@@ -1,8 +1,21 @@
 (setq org-startup-indented t)
 
-(setq org-todo-keywords '((sequence "IDEA" "TODO" "WIP" "DELEGATED" "|" "DONE")))
+(setq org-todo-keywords '((sequence "TODO(t)" "DONE(d)")
+                          (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE" "|" "FIXED(f)")))
 
 (defun gtd ()
    (interactive)
    (find-file "~/Documents/Notes/org/gtd.org")
  )
+
+
+
+;;(setq org-default-notes-file (concat org-directory "~/Documents/Notes/org/notes.org")) ;
+(define-key global-map "\C-cc" 'org-capture)
+
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/Documents/Notes/org/gtd.org" "Tasks")
+         "* TODO %?\n  %i\n  %a
+")
+        ("j" "Journal" entry (file+datetree "~/Documents/Notes/org/journal.org")
+         "* %?\nEntered on %U\n  %i\n  %a")))
