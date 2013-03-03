d5b8ad42e8641f53345d9f8cc0ea339fcc6b4900
at <at@an-ti.eu>
2013-03-03 12:59:34 +0100

    adding Rakefiles to ruby-mode autolad

--- etc/ruby-mode.el
+++ etc/ruby-mode.el
@@ -2,6 +2,7 @@
 
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("/Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("/Rakefile$" . ruby-mode))
 
 (eval-after-load "ruby-mode"
   '(progn
