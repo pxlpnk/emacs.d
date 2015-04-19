83deb88284f14e12ca6805a4c9c83a574cc7385f
Andreas Tiefenthaler <at@an-ti.eu>
2015-04-19 16:02:46 +0700

    Rename function prefix mk- -> at-

--- init.el
+++ init.el
@@ -3,30 +3,32 @@
 
 
 ;; Directories and file names
-(setq mk-emacs-init-file
+(setq at-emacs-init-file
       (or load-file-name buffer-file-name))
-(setq mk-emacs-config-dir
-      (file-name-directory mk-emacs-init-file))
-(setq user-emacs-directory mk-emacs-config-dir)
-(setq mk-elisp-dir
-      (expand-file-name "elisp" mk-emacs-config-dir))
-(setq mk-elisp-external-dir
-      (expand-file-name "external" mk-elisp-dir))
-(setq mk-themes-dir
-      (expand-file-name "themes" mk-elisp-dir))
-(setq mk-init-dir
-      (expand-file-name "init.d" mk-emacs-config-dir))
+(setq at-emacs-config-dir
+      (file-name-directory at-emacs-init-file))
+(setq user-emacs-directory at-emacs-config-dir)
+(setq at-elisp-dir
+      (expand-file-name "elisp" at-emacs-config-dir))
+(setq at-elisp-external-dir
+      (expand-file-name "external" at-elisp-dir))
+(setq at-themes-dir
+      (expand-file-name "themes" at-elisp-dir))
+(setq at-init-dir
+      (expand-file-name "init.d/" at-emacs-config-dir))
+
+;; TODO: add possibility to exclude files when loading from init.d
 
 ;; Load all elisp files in ./init.d
-(if (file-exists-p mk-init-dir)
-    (dolist (file (directory-files mk-init-dir t "\\.el$"))
+(if (file-exists-p at-init-dir)
+    (dolist (file (directory-files at-init-dir t "\\.el$"))
       (load file)))
 
 (setq dotfiles-dir (file-name-directory
                     (or (buffer-file-name) load-file-name)))
 ;; Write backup files to own directory
 (setq backup-directory-alist
-      `(("." . ,(expand-file-name (concat mk-emacs-config-dir "bak")))))
+      `(("." . ,(expand-file-name (concat at-emacs-config-dir "bak")))))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
@@ -35,16 +37,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
- '(custom-safe-themes
-   (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(package-selected-packages
    (quote
-    (erlang yasnippet-bundle undo-tree smartparens robe rbenv rainbow-delimiters markdown-mode enh-ruby-mode direx dash-at-point company clojure-snippets cider bubbleberry-theme))))
+    (color-theme-sanityinc-tomorrow yasnippet-bundle undo-tree smartparens robe rbenv rainbow-delimiters markdown-mode helm erlang enh-ruby-mode dash-at-point company clojure-snippets cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-comment-face ((t (:height 0.8)))))
+ )
