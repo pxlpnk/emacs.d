c8b515d938d2feadd3a965e263473ebecf9fc4ad
Andreas Tiefenthaler <at@an-ti.eu>
2016-03-17 23:23:07 +0100

    Clean projectile.el

--- init.d/70-projectile.el
+++ init.d/70-projectile.el
@@ -1,4 +1,6 @@
+(require 'projectile)
 (projectile-global-mode)
+
 (setq projectile-completion-system 'helm)
 (helm-projectile-on)
 
@@ -6,8 +8,6 @@
 (global-set-key (kbd "H-b") 'helm-projectile-switch-to-buffer)
 (global-set-key (kbd "H-f") 'at/helm-projectile-ag)
 
-(require 'projectile)
-(projectile-global-mode)
 ;; (setq projectile-enable-caching t)
 (setq projectile-indexing-method 'alien)
 (setq projectile-completon-system 'helm)
