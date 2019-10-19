dd450119e0ce4bb329711fc73d5f7275256406f2
Andreas Tiefenthaler <at@an-ti.eu>
2019-10-19 14:25:49 +0700

    Package stuff

--- init.d/packages.el
+++ init.d/packages.el
@@ -17,9 +17,13 @@
     (require 'smartparens-config)
     (smartparens-global-mode 1)
     (smartparens-global-strict-mode 1)))
+
 (use-package rainbow-delimiters)
 
 (use-package helm)
+(use-package helm-ag
+  :ensure t)
+
 (use-package projectile)
 (use-package undo-tree)
 (use-package magit)
@@ -40,9 +44,43 @@
   :config
   (progn (beacon-mode 1)))
 
-
 (use-package yasnippet)
 (use-package yasnippet-snippets)
 
-
 (use-package go-mode)
+(use-package go-eldoc
+  :ensure t
+  :config
+  (progn (add-hook 'go-mode-hook 'go-eldoc-setup)))
+(use-package yaml-mode)
+
+(use-package flycheck
+  :ensure t
+  :config
+  :init (global-flycheck-mode))
+
+
+(use-package yasnippet
+  :ensure t
+  :config
+
+  (yas-global-mode t)
+  (define-key yas-minor-mode-map (kbd "<tab>") nil)
+  (define-key yas-minor-mode-map (kbd "C-'") #'yas-expand)
+
+  (yas-reload-all)
+  (setq yas-prompt-functions '(yas-ido-prompt))
+  (defun help/yas-after-exit-snippet-hook-fn ()
+    (prettify-symbols-mode)
+    (prettify-symbols-mode))
+  (add-hook 'yas-after-exit-snippet-hook #'help/yas-after-exit-snippet-hook-fn)
+  :diminish yas-minor-mode)
+
+
+(use-package yasnippet-snippets         ; Collection of snippets
+  :ensure t)
+
+(use-package editorconfig
+  :ensure t
+  :config
+  (editorconfig-mode 1))
