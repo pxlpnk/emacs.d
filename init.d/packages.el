5ce12330ab5f60769aa2a15abf1c7d4e86111435
Andreas Tiefenthaler <at@an-ti.eu>
2020-03-04 10:02:42 +0700

    Remove org-roam for now

--- init.d/packages.el
+++ init.d/packages.el
@@ -153,18 +153,3 @@
 (use-package rubocopfmt)
 (use-package use-package)
 (use-package hcl-mode)
-
-
-(use-package org-roam
-      :after org
-      :hook
-      (after-init . org-roam-mode)
-      :straight (:host github :repo "jethrokuan/org-roam" :branch "develop")
-      :custom
-      (org-roam-directory "~/org/")
-      :bind (:map org-roam-mode-map
-              (("C-c n l" . org-roam)
-               ("C-c n f" . org-roam-find-file)
-               ("C-c n g" . org-roam-show-graph))
-              :map org-mode-map
-              (("C-c n i" . org-roam-insert))))
